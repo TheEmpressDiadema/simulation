ef2d61015985b51d5c0e2d30e5ac24d25ecf89cb
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-12-28 20:15:02 +0300

    feat: factory classes to implement

--- src/simulation/creators/entity_creator.py
+++ src/simulation/creators/entity_creator.py
@@ -1,25 +1,7 @@
-from simulation.config.config import (
-    GRASS_PROBABILITY, TREE_PROBABILITY, ROCK_PROBABILITY,
-    HERBIVORE_PROBABILITY, PREDATOR_PROBABILITY,
-    HERBIVORE_HP, HERBIVORE_SPEED,
-    PREDATOR_HP, PREDATOR_SPEED, PREDATOR_DAMAGE
-)
-from simulation.entities.creatures import (
-    Herbivore, Predator
-)
-from simulation.entities.entity import (
-    Grass, Rock, Tree
-)
+from abc import ABC, abstractmethod
 
-class EntityCreator:
 
-    _entities = {
-        GRASS_PROBABILITY : Grass(),
-        TREE_PROBABILITY : Tree(),
-        ROCK_PROBABILITY : Rock(),
-        HERBIVORE_PROBABILITY : Herbivore(),
-        PREDATOR_PROBABILITY : Predator(),
-    }
+class Factory(ABC):
 
-    def create_entity(self):
-        return 
+    def __getitem__(self):
+        return self
