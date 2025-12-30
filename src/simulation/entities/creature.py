b642ef2b9a2c3df0d656661a8af7dbff4533ce15
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-12-30 16:44:24 +0300

    BREAKING CHANGE: creatures package renamed as creature, creature doesn't have position property anymore

--- src/simulation/entities/creature.py
+++ src/simulation/entities/creature.py
@@ -10,18 +10,9 @@ class Creature(Entity):
     
     target_type: type[Resource]
 
-    def __init__(self, position: Coordinate, hp: int, speed: int):
-        self._position = position
+    def __init__(self, hp: int, speed: int):
         self._hp = hp
         self._speed = speed
-
-    @property
-    def position(self) -> Coordinate:
-        return self._position
-    
-    @position.setter
-    def position(self, cell: Coordinate):
-        self._position = cell
 
     @property
     def hp(self) -> int:
@@ -35,31 +26,30 @@ class Creature(Entity):
     def speed(self) -> int:
         return self._speed
 
+    @abstractmethod
+    def eat(self, target: Entity):
+        pass
+
     def make_move(self, path: list[Coordinate], field: Map):
-        move_count = 0
+        prev_cell = None
         for cell in path:
             content = field[cell]
             if isinstance(content, self.target_type):
                 self.eat(content)
                 if content.eaten:
-                    self.position = cell
-            else:
-                self.position = cell
-                move_count += 1
-                if move_count == self.speed:
-                    break
-
-    @abstractmethod
-    def eat(self, target: Entity):
-        pass
+                    field.delete_entity(cell)
+            if field[cell] is None:
+                field[cell] = self
+                field[prev_cell] = None
+            prev_cell = cell
 
 class Herbivore(Creature, Resource):
     
     icon: str = HERBIVORE_ICON
     target_type: type[Resource] = Grass
 
-    def __init__(self, position, hp, speed):
-        Creature.__init__(self, position, hp, speed)
+    def __init__(self, hp: int, speed: int):
+        Creature.__init__(self, hp, speed)
         Resource.__init__(self)
 
     def eat(self, target: Entity):
@@ -72,8 +62,8 @@ class Predator(Creature):
     icon: str = PREDATOR_ICON
     target_type: type[Resource] = Herbivore
 
-    def __init__(self, position: Coordinate, hp: int, speed: int, damage: int):
-        super().__init__(position, hp, speed)
+    def __init__(self, hp: int, speed: int, damage: int):
+        super().__init__(hp, speed)
         self._damage = damage
 
     def eat(self, target: Entity):
