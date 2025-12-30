a41f48aa61035f6d1ef6dda480510ad2a0780db7
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-12-30 16:38:51 +0300

    fix: some testing code for current project stage

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -1,5 +1,6 @@
 from simulation.actions.action import MoveCreature
-from simulation.entities.creatures import Herbivore, Predator
+from simulation.entities.creature import Herbivore, Predator
+from simulation.entities.entity import Grass
 from simulation.map.coordinate import Coordinate
 from simulation.map.map import Map
 from simulation.view.renderer import Renderer
@@ -10,16 +11,16 @@ def main() -> None:
     renderer = Renderer()
     renderer.print_field(field)
     print()
-    p1 = Predator(Coordinate(1,1), PREDATOR_HP, PREDATOR_SPEED, PREDATOR_DAMAGE)
+    p1 = Predator(PREDATOR_HP, PREDATOR_SPEED, PREDATOR_DAMAGE)
     field[Coordinate(1,1)] = p1
     renderer.print_field(field)
-    p2 = Herbivore(Coordinate(2,2), HERBIVORE_HP, HERBIVORE_SPEED)
+    p2 = Herbivore(HERBIVORE_HP, HERBIVORE_SPEED)
     field[Coordinate(2,2)] = p2
-    print()
+    p3 = Grass()
+    field[Coordinate(3,3)] = p3
     renderer.print_field(field)
-    print()
-    act = MoveCreature()
-    act.run(field)
+    move_action = MoveCreature()
+    move_action.run(field)
     renderer.print_field(field)
-    print()
-    print(p2.hp)
+
+main()
