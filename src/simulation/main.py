495bf667c25d98ec59dabf80c73b59bedd6634bf
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-12-30 23:39:37 +0300

    BREAKING CHANGE: changed testing code due to project changes

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -1,26 +1,28 @@
-from simulation.actions.action import MoveCreature
-from simulation.entities.creature import Herbivore, Predator
-from simulation.entities.entity import Grass
-from simulation.map.coordinate import Coordinate
+import time
+
+from simulation.entities.creature import Herbivore
 from simulation.map.map import Map
+
+from simulation.actions.action import SpawnEntity, MoveCreature
 from simulation.view.renderer import Renderer
-from simulation.config.config import *
+from simulation.config.config import MAP_HEIGHT, MAP_WIDTH
 
 def main() -> None:
     field = Map(MAP_HEIGHT, MAP_WIDTH)
-    renderer = Renderer()
-    renderer.print_field(field)
-    print()
-    p1 = Predator(PREDATOR_HP, PREDATOR_SPEED, PREDATOR_DAMAGE)
-    field[Coordinate(1,1)] = p1
-    renderer.print_field(field)
-    p2 = Herbivore(HERBIVORE_HP, HERBIVORE_SPEED)
-    field[Coordinate(2,2)] = p2
-    p3 = Grass()
-    field[Coordinate(3,3)] = p3
-    renderer.print_field(field)
+    spawn_action = SpawnEntity()
     move_action = MoveCreature()
-    move_action.run(field)
-    renderer.print_field(field)
+    renderer = Renderer()
+    while True:
+        spawn_action.run(field)
+        renderer.print_field(field)
+        print("spawned")
+        move_action.run(field)
+        renderer.print_field(field)
+        print("moved all")
+        entities = field.get_entity_cells()
+        for entity in entities:
+            if isinstance(field[entity], Herbivore):
+                print(field[entity].hp)
+        time.sleep(3)
 
 main()
