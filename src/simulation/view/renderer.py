0e7d0e76b17064f9db06768eecd54f6a16faf812
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-12-31 10:12:56 +0300

    feat: clear console method implemented

--- src/simulation/view/renderer.py
+++ src/simulation/view/renderer.py
@@ -1,3 +1,6 @@
+import os
+
+
 from simulation.config.config import FREE_CELL_ICON
 from simulation.map.coordinate import Coordinate
 from simulation.map.map import Map
@@ -6,7 +9,8 @@ from simulation.map.map import Map
 class Renderer:
 
     def print_field(self, game_map: Map):
-        
+        self._clear_console()
+
         for row in range(1, game_map.height+1):
             for col in range(1, game_map.width+1):
                 self._print_cell(game_map, Coordinate(row, col))
@@ -18,3 +22,9 @@ class Renderer:
             print(FREE_CELL_ICON, end='')
         else:
             print(game_map[coordinate].icon, end='')
+    
+    def _clear_console(self):
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
