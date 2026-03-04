ad20dd3a75c9722ad5b4ee848588b526d6ee5d3a
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2026-03-04 22:36:50 +0300

    feat: simulation class to run simulation

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -1,37 +1,59 @@
-import sys
 import time
-import threading     
+import threading  
 
-def run_game(event: threading.Event):
-    while True:
-        event.wait()
-        print("Игра играется...")
-        time.sleep(1)
+from simulation.map.map import Map   
+from simulation.config.config import MAP_WIDTH, MAP_HEIGHT
+from simulation.actions.action import MoveCreature, SpawnEntity
+from simulation.view.menu import Menu
+from simulation.view.renderer import Renderer
 
-def process_input(event: threading.Event):
-    while True:
-        cmd = input().strip().lower()
-        if cmd == "p":
-            event.clear()
-            print("Игра на паузе")
-        if cmd == "c":
-            event.set()
-            print("Игра продолжается")
-        if cmd == "e":
-            sys.exit(0)
+class Simulation:
+
+    def __init__(self):
+        self._move_counter = 0
+        self._renderer = Renderer()
+        self._menu_view = Menu()
+        self._map = Map(MAP_HEIGHT, MAP_WIDTH)
+        self._actions = [SpawnEntity(), MoveCreature()]
+        self._stop_flag = False
+        self._paused = False
+
+    def run(self):
+        input_thread = threading.Thread(target=self._input_listener, daemon=True)
+        input_thread.start()
+
+        self._menu_view.print_view()
+
+        while not self._stop_flag:
+            while self._paused and not self._stop_flag:
+                time.sleep(0.1) 
+            if self._stop_flag:
+                break
+
+            for action in self._actions:
+                action.run(self._map)
+
+            self._move_counter += 1
+            self._renderer.print_field(self._map)
+
+            time.sleep(1)
+
+        print(f"Симуляция остановлена. Всего ходов: {self._move_counter}")
+
+    def _input_listener(self):
+        while not self._stop_flag:
+            cmd = input().strip().lower()
+            if cmd == 'q':
+                self._stop_flag = True
+                break
+            elif cmd == 'p':
+                self._paused = True
+                print("Пауза. Введите 'c' для продолжения.")
+            elif cmd == 'c':
+                self._paused = False
+                print("Продолжение.")
 
 
 
 def main() -> None:
-
-    event = threading.Event()
-    event.set()
-
-    thread1 = threading.Thread(target=run_game, args=(event,))
-    thread2 = threading.Thread(target=process_input, args=(event,), daemon=True)
-
-    thread1.start()
-    thread2.start()
-
-    thread1.join()
-    thread2.join()
+    Simulation().run()
--- src/simulation/view/menu.py
+++ src/simulation/view/menu.py
@@ -2,4 +2,4 @@
 class Menu:
 
     def print_view(self):
-        print("1.Start\n2.Stop\n3.Exit")
+        print("p - Stop\nq - Exit\nc - continue")
